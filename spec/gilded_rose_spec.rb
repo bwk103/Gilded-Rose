7c8bd23d528f379592118593c7d970c62f22c1ff
Ben Kielty <bwk103@gmail.com>
2018-03-14 08:48:39 +0000

    Add Unit tests for Sufuras and Brie

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,12 +2,36 @@ require 'gilded_rose'
 
 describe GildedRose do
 
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+  let(:sulfuras) { Item.new('Sulfuras, Hand of Ragnaros', 0, 80) }
+  let(:brie) { Item.new('Aged Brie', 10, 10) }
+  let(:passes) { Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 10) }
+
+  describe '#update_quality' do
+    describe 'Sulfuras, Hand of Ragnaros' do
+      before(:each) do
+        @items = [sulfuras]
+        GildedRose.new(@items).update_quality()
+      end
+      it 'quality does not decrease' do
+        expect(@items[0].quality).to eq 80
+      end
+
+      it 'sellIn does not decrease' do
+        expect(@items[0].sell_in).to eq 0
+      end
+    end
+
+    describe 'Aged Brie' do
+      before(:each) do
+        @items = [brie]
+        GildedRose.new(@items).update_quality()
+      end
+      it 'quality increases by 1' do
+        expect(@items[0].quality).to eq 11
+      end
+      it 'sellIn decreases by 1' do
+        expect(@items[0].sell_in).to eq 9
+      end
     end
   end
-
 end
