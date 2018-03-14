c29fb1a864691287495a163ae47816f750a5f635
Ben Kielty <bwk103@gmail.com>
2018-03-14 11:15:49 +0000

    Further refactoring of #update_quality

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,15 +1,20 @@
 class GildedRose
 
+  MAX_QUALITY = 50
+  MIN_QUALITY = 0
+
   def initialize(items)
     @items = items
   end
 
   def update_quality()
     @items.each do |item|
-      if item.name == "Sulfuras, Hand of Ragnaros"
-      elsif item.name == "Aged Brie"
+      case item.name
+      when "Sulfuras, Hand of Ragnaros"
+        item
+      when "Aged Brie"
         aged_brie(item)
-      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+      when "Backstage passes to a TAFKAL80ETC concert"
         backstage_passes(item)
       else
         original_item(item)
@@ -21,13 +26,13 @@ class GildedRose
     item.quality -= 1
     item.sell_in -= 1
     item.quality -= 1 if item.sell_in < 0
-    item.quality = 0 if item.quality < 0
+    checkMinQuality(item)
   end
 
   def aged_brie(item)
     item.quality += 1
     item.sell_in -= 1
-    item.quality = 50 if item.quality > 50
+    checkMaxQuality(item)
   end
 
   def backstage_passes(item)
@@ -35,8 +40,16 @@ class GildedRose
     item.quality += 1 if item.sell_in <= 10
     item.quality += 1
     item.sell_in -= 1
-    item.quality = 50 if item.quality > 50
     item.quality = 0 if item.sell_in <= 0
+    checkMaxQuality(item)
+  end
+
+  def checkMaxQuality(item)
+    item.quality = MAX_QUALITY if item.quality > MAX_QUALITY
+  end
+
+  def checkMinQuality(item)
+    item.quality = MIN_QUALITY if item.quality < MIN_QUALITY
   end
 
 end
