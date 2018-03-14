811aedd584e949d1a70338898e53c6da82ec35e9
Ben Kielty <bwk103@gmail.com>
2018-03-14 11:01:16 +0000

    First sweep refactor #updateQuality

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,52 +6,39 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      item if item.name == "Sulfuras, Hand of Ragnaros"
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+      if item.name == "Sulfuras, Hand of Ragnaros"
+      elsif item.name == "Aged Brie"
+        aged_brie(item)
+      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+        backstage_passes(item)
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+        original_item(item)
       end
     end
   end
+
+  def original_item(item)
+    item.quality -= 1
+    item.sell_in -= 1
+    item.quality -= 1 if item.sell_in < 0
+    item.quality = 0 if item.quality < 0
+  end
+
+  def aged_brie(item)
+    item.quality += 1
+    item.sell_in -= 1
+    item.quality = 50 if item.quality > 50
+  end
+
+  def backstage_passes(item)
+    item.quality += 1 if item.sell_in <= 5
+    item.quality += 1 if item.sell_in <= 10
+    item.quality += 1
+    item.sell_in -= 1
+    item.quality = 50 if item.quality > 50
+    item.quality = 0 if item.sell_in <= 0
+  end
+
 end
 
 class Item
